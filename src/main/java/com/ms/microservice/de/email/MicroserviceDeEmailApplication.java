52ed6fa37baf2c6c5aa0003d8070a40108fa4620
victornext <victorbrs517@gmail.com>
2026-02-03 16:54:03 -0300

    base

---
+++ src/main/java/com/ms/microservice/de/email/MicroserviceDeEmailApplication.java
@@ -0,0 +1,13 @@
+package com.ms.microservice.de.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDeEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDeEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/microservice/de/email/MicroserviceDeEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.microservice.de.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDeEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
